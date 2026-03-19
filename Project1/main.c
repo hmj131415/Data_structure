1462f804eb904517ad6daae481cdd25d4730bd05
hmj131415 <1470501124@qq.com>
2026-03-19 17:43:54 +0800

    内核链表的增删及节点剔除，遍历与节点移动

--- Project1/main.c
+++ Project1/main.c
@@ -1,99 +1,144 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include "kernel_list.h"
 
-typedef struct student
+//定义链表节点结构体
+typedef struct node
 {
-    char name[20];
-    int age;
-}student;
-//定义链表存放数据类型为struct student
-#define DATATYPE student
-#include "list.h"
+    /* data */
+    int data;
+    struct list_head list;
+}list_node,*linklist;
 
-//定义显示学生信息的函数
-void showinfo(student *structdata)
+//初始化链表头结点
+linklist list_init()
 {
-    printf("name = %s,age = %d\n",structdata->name,structdata->age);
-    return;
-}
-
-//显示链表中整数
-void showint(int *data)
-{
-    printf("%d\n",*data);
-    return;
-}
-
-//定义判断两个学生是否相等的函数
-
-bool equal_student(student data,student finddata)
-{
-    if(strcmp(data.name,finddata.name) == 0 && data.age == finddata.age)
+    linklist head = (linklist)malloc(sizeof(list_node));
+    if (head!=NULL)
     {
-        return true;
+        /* code */
+        INIT_LIST_HEAD(&(head->list));
     }
-    return false;
+    return head;
+}
+ 
+//生成int类型内核节点
+linklist generate_int_node(int data)
+{
+    linklist node = (linklist)malloc(sizeof(list_node));
+    if (node!=NULL)
+    {
+        /* code */
+        node->data = data;
+        INIT_LIST_HEAD(&(node->list)); 
+    }
+    return node;
 }
 
-
-bool equal_int(int data,int finddata)
+//查找指定int节点
+linklist find_node(linklist head,int data)
 {
-    if(data == finddata)
+    linklist p;
+    list_for_each_entry(p, &(head->list), list)
     {
-        return true;
+        if (p->data==data)
+        {
+            return p;
+        }
     }
-    return false;
+    return NULL; 
+}
+
+//从前往后打印链表节点数据
+void print_list(linklist head)
+{
+    struct list_head *pos;
+    list_for_each(pos, &(head->list))
+    {
+        linklist entry = list_entry(pos,list_node,list);   
+        printf("%d", entry->data);
+    }
+    printf("\n");
+}
+
+void node_move(linklist head,int move_data,int target_data)
+{
+    linklist move_node = find_node(head,move_data);
+    linklist target_node = find_node(head,target_data);
+    if (move_node!=NULL&&target_node!=NULL)
+    {
+        list_del(&(move_node->list));
+        list_add(&(move_node->list), &(target_node->list));
+    }
+    else
+    {
+        printf("移动失败，未找到节点%d或节点%d\n",move_data,target_data);
+    }
 }
 
 int main()
 {
-    //初始化空链表
-    linklist head = initlist();
-
-    //创建学生新节点
-    
-    student Jack = {"Jack",18};
-    student Tom = {"Tom",19};
-    student Rose = {"Rose",20};
-    linklist newnode1 = createnode(&Jack);
-    linklist newnode2 = createnode(&Tom);
-    linklist newnode3 = createnode(&Rose);
-    
-
-
-    //将新节点插入到链表末尾
-    listaddtail(head,newnode1);
-    listaddtail(head,newnode2);
-    listaddtail(head,newnode3);
-   
-
-    //创建10个整数新节点
-    /* for (int i = 0; i < 10; i++)
+    //初始化内核链表头结点
+    linklist head = list_init();
+    if (head!=NULL)
     {
-        linklist newnode = createnode(&i);
-        listaddtail(head,newnode);
-    } */
-    
-
-    //遍历整个链表，每个节点调用showinfo函数显示信息
-    listforeach(head,showinfo);
-
-    //查找链表中的指定数据的节点
-    linklist p = findnode(head,Tom,equal_student);
-    if (p!=NULL)
-    {
-        printf("找到了\n");
+        /* code */
+        printf("list_init success\n");
     }
     else
     {
-        printf("没有找到\n");
+        printf("list_init failed\n");
     }
 
-    //删除指定数据的节点
-    listdelnode(head,p);
-    free(p);
-    //遍历整个链表，每个节点调用showinfo函数显示信息
-    listforeach(head,showinfo);
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+        linklist node = generate_int_node(i);
+        //在链表头结点后面添加节点node
+        // list_add(&(node->list), &(head->list));
+        //在链表头结点后面添加节点node
+        list_add_tail(&(node->list), &(head->list));
+    }
     
+    
+    struct list_head *pos;
+
+    //从前往后遍历链表，打印节点数据
+    print_list(head);
+    
+
+    //删除指定循环
+    /* char stop;
+    int delete_data;
+    printf("请输入要删除的节点数据,输入q退出：");
+    while (1)
+    {
+        if (scanf("%c",&stop)==1&&(stop=='q'||stop=='Q'))
+        {
+            
+            break;
+        }
+        else if (scanf("%d",&delete_data)==1)
+        {
+            
+            linklist p = find_node(head,delete_data);
+            if (p!=NULL)
+            {
+                list_del(&(p->list));
+                free(p);
+                print_list(head);
+            }
+            else
+            {
+                printf("删除失败，未找到节点%d\n",delete_data);
+            }
+        }
+       
+    } */
+    
+    //移动节点（把第一个参数移到第二个参数后面）
+    node_move(head,7,1);
+    print_list(head);
     return 0;
 }
+
